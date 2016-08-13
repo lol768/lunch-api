1541c36f4ce678b0d352c718f1f69b2882f5e984
Bobby Dilley <bobby@netcraft.com>
2016-08-13 01:58:38 +0100

    Add automatic deployment code

---
+++ deploy.php
@@ -0,0 +1,17 @@
+<?
+// Github Webhook deployment Code
+
+// Swap the path to the directory of our git project
+$path = "/var/www/henrythompson.me/lunchApi"; 
+chdir( $path );
+
+// Now run the git stuf
+exec( 'git fetch --all' );
+exec( 'git reset --hard origin/master' );
+
+// Probably done so just return a fun true response
+header( 'Content-type: application/json' );
+$response = array( "status" => "success" );
+echo json_encode( $response );
+
+?>
