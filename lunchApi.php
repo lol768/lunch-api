7562a07056a177b8b0fb1824eb1e7bf5e3cd0fe6
Bobby Dilley <bobby@netcraft.com>
2016-08-13 01:57:17 +0100

    Added in framework for more lunchTime options

--- lunchApi.php
+++ lunchApi.php
@@ -1,4 +1,18 @@
 <?php
+
+// Lunch API Version 0.1
+
+
 date_default_timezone_set('Europe/London');
+
 header("Content-Type: application/json");
-die(json_encode(["isLunch" => date('H') == 12]));
+
+// Setup the outputs
+$isLunch = (date('H') == 12);
+$lunchTime = "12:00";
+
+$response = array("lunchTime" => $lunchTime, "isLunch" => $isLunch)
+
+die(json_encode($response));
+
+?>
