6e097e9b547d3c31cecd80e6a221b21d84de0b86
Bobby Dilley <bobby@netcraft.com>
2016-08-17 13:15:09 +0100

    Add the lunch type variable

--- api/v1/lunch/index.php
+++ api/v1/lunch/index.php
@@ -10,8 +10,9 @@ header("Content-Type: application/json");
 // Setup the outputs
 $isLunch = (date('H') == 12);
 $lunchTime = "12:00";
+$lunchType = "Unknown";
 
-$response = array("lunchTime" => $lunchTime, "isLunch" => $isLunch);
+$response = array("lunchTime" => $lunchTime, "isLunch" => $isLunch, "lunchType" => $lunchType);
 
 die(json_encode($response));
 
